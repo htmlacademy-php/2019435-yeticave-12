fd1706fb2c1913cfec73ef8a747a88f18717f4c1
Pavel-kld <rezokold@gmail.com>
2022-02-20 13:38:22 +0300

    Внесение правок по комментариям

--- index.php
+++ index.php
@@ -35,10 +35,10 @@ $user_name = ''; // укажите здесь ваше имя
 </header>
 
 <?php
-$staff = [
+$categories = [
     "Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"
     ];
-$advert = [
+$adverts = [
     [
         'name' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
@@ -84,9 +84,9 @@ $advert = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($staff as $val): ?>
+            <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $val ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $category ?></a>
             </li>
             <? endforeach; ?>
         </ul>
@@ -97,18 +97,18 @@ $advert = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($advert as $key => $val): ?>
+            <?php foreach ($adverts as $advert): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $val['url'] ?>" width="350" height="260" alt="">
+                    <img src="<?= $advert['url'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $val['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $val['name'] ?></a></h3>
+                    <span class="lot__category"><?= $advert['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $advert['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount"><?= $val['price'] ?></span>
-                            <span class="lot__cost"><?= $val['price'] ?><b class="rub"></b></span>
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?= $advert['price'] ?><b class="rub"></b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -126,9 +126,9 @@ $advert = [
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($staff as $val): ?>
+            <?php foreach ($categories as $category): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?= $val ?></a>
+                <a href="pages/all-lots.html"><?= $category ?></a>
             </li>
             <? endforeach; ?>
         </ul>
