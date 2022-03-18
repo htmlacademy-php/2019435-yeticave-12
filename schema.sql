8b32878905637f74fb4257cb7823226623ad68e3
Pavel-kld <rezokold@gmail.com>
2022-03-18 21:14:57 +0300

    Исправление базы данных (18.03.2022)

--- schema.sql
+++ schema.sql
@@ -6,24 +6,24 @@ USE yeti;
 
 CREATE TABLE category
 (
-  id          INT AUTO_INCREMENT PRIMARY KEY,
+  id          INT(11) AUTO_INCREMENT PRIMARY KEY,
   name        CHAR(10) NOT NULL,
   symbol_code CHAR(10) UNIQUE NOT NULL
 );
 
 CREATE TABLE lot
 (
-  id            INT AUTO_INCREMENT PRIMARY KEY,
+  id            INT(11) AUTO_INCREMENT PRIMARY KEY,
   creation_date DATETIME NOT NULL ,
   name          CHAR(10) NOT NULL,
   description   TEXT NOT NULL,
   img_source    CHAR(20) NOT NULL,
   price         REAL NOT NULL,
   end_date      CHAR(10) NOT NULL,
-  bid_step      INT NOT NULL,
-  author        CHAR(10) NOT NULL,
-  winner        CHAR(10) NULL,
-  category      CHAR(10) NOT NULL
+  bid_step      INT(11) NOT NULL,
+  author_id     INT(11) NOT NULL,
+  winner_id     INT(11) NULL,
+  category_id   INT(11) NOT NULL
 );
 
 CREATE INDEX creation_date ON lot (creation_date);
@@ -31,21 +31,19 @@ CREATE INDEX end_date ON lot (end_date);
 
 CREATE TABLE bid
 (
-  id    INT AUTO_INCREMENT PRIMARY KEY,
+  id    INT(11) AUTO_INCREMENT PRIMARY KEY,
   date  DATETIME NOT NULL,
   price REAL NOT NULL,
-  user  CHAR(10) NOT NULL,
-  lot   CHAR(10) NOT NULL
+  user_id  INT(11) NOT NULL,
+  lot_id   INT(11) NOT NULL
 );
 
 CREATE TABLE user
 (
-  id                INT AUTO_INCREMENT PRIMARY KEY,
+  id                INT(11) AUTO_INCREMENT PRIMARY KEY,
   registration_date DATETIME NOT NULL,
   email             CHAR(15) NOT NULL,
   name              CHAR(10) NOT NULL,
   password          CHAR(40) NOT NULL,
-  contacts          TEXT NOT NULL,
-  created_lots      TEXT NULL,
-  created_bids      TEXT NULL
+  contacts          TEXT NOT NULL
 );
